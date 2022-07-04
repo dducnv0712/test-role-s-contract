0ae73678c285faed69193210e7c6be0634371e64
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-07-04 16:19:47 +0700

    thu dia chi

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,20 +13,22 @@ class ApiController extends Controller
         return response()->json([
             "ip"=>$request->ip(),
             'location'=> Location::get($request->ip()),
-            'platform'=>$request->headers->get("sec-ch-ua-platform")
+            'platform'=>$request->headers->get("sec-ch-ua-platform"),
+            'redirect'=>$request->intended()
         ],200);
     }
     //
-//    public function imageBase64(Request $request)
-//    {
-//        $image = $request->image;
-//        preg_match("/data:image\/(.*?);/",$image,$image_extension); // extract the image extension
-//        $image = preg_replace('/data:image\/(.*?);base64,/','',$image); // remove the type part
-//        $image = str_replace(' ', '+', $image);
-//        $imageName = 'image_' . time() . '.' . $image_extension[1]; //generating unique file name;
-//        Storage::disk('public')->put($imageName,base64_decode($image));
-//        return response()->json([
-//            "message"=>"rat thanh cong"
-//        ], 200);
-//    }
+    public function imageBase64(Request $request)
+    {
+        $image = $request->image;
+        preg_match("/data:image\/(.*?);/",$image,$image_extension); // extract the image extension
+        $image = preg_replace('/data:image\/(.*?);base64,/','',$image); // remove the type part
+        $image = str_replace(' ', '+', $image);
+        $imageName = 'image_' . time() . '.' . $image_extension[1]; //generating unique file name;
+       $path =  Storage::disk('public')->put($imageName,base64_decode($image));
+        return response()->json([
+            "message"=>"rat thanh cong",
+            "path"=>$imageName
+        ], 200);
+    }
 }
